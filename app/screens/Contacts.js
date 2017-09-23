ddecc1c2e5d91a4f63b89497f4ee27a329ac3039
Pardip <pardipbhatti@gpcoders.com>
2017-09-23 16:16:03 +0530

    init app

---
+++ app/screens/Contacts.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { FlatList } from 'react-native';
+
+import { contacts } from '../config/data';
+import colors from '../config/colors';
+import { ListItem } from '../components/ListItem';
+
+class Contacts extends Component {
+
+    handleRowPress = (item) => {
+        return null;
+    }
+
+    render(){
+        return(
+            <FlatList
+                style={{ backgroundColor: colors.background }}
+                data={contacts}
+                renderItem={({ item }) =>
+                    <ListItem contact={item} onPress={() => this.handleRowPress(item)} />
+                }
+                keyExtractor={(item) => item.email}
+            />
+        )
+    }
+}
+export default Contacts;
